21c979199e3d53673165d3fa0e890444f7d69c02
Danil Stafeychuk <danil_stafeychuk_97@mail.ru>
2024-01-14 19:37:36 +0700

    Add files via upload

--- mailing.py
+++ mailing.py
@@ -1,7 +1,11 @@
 class Mailing:
-    from adress import Adress
 
-    to_adress = Adress
-    from_adress = Adress
-    cost = 100
-    track = '12345678901234'
+    def __init__(self, track, cost):
+        self.cost = cost
+        self.track = track
+
+    def say_track(self):
+        print('Отправление', self.track, 'из')
+
+    def say_cost(self):
+        print('Стоимость', self.cost, 'рублей.')
